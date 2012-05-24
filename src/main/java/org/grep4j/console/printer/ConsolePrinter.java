f68c872a43765f2791e58a771cef41fc211f42ae
marcocast <marcocast@gmail.com>
2012-05-24 17:00:20 +0100

    change

--- src/main/java/org/grep4j/console/printer/ConsolePrinter.java
+++ src/main/java/org/grep4j/console/printer/ConsolePrinter.java
@@ -2,7 +2,7 @@ package org.grep4j.console.printer;
 
 import static org.grep4j.console.persistence.profiles.ProfileConfiguration.profileConfiguration;
 
-import java.util.List;
+import java.util.Set;
 
 import org.grep4j.core.task.GrepResult;
 
@@ -11,7 +11,7 @@ public class ConsolePrinter {
 	private final static String AROUND_HEADER = "************************************************";
 	private final static String AROUND_CONTENT = "					";
 
-	public static void printResult(List<GrepResult> results) {
+	public static void printResult(Set<GrepResult> results) {
 		for (GrepResult result : results) {
 			System.out.println(AROUND_HEADER + AROUND_HEADER + AROUND_HEADER);
 			System.out.println(AROUND_CONTENT + result.getProfileName() + " {" + result.getFileName() + " [" + hostFor(result) + "]}"
