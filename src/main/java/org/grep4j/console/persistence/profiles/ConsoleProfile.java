d20eb8d69ad9ff7035464ccfc3ca637bd9499956
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2012-05-28 23:53:56 +0100

    Added Console Profile

---
+++ src/main/java/org/grep4j/console/persistence/profiles/ConsoleProfile.java
@@ -0,0 +1,56 @@
+package org.grep4j.console.persistence.profiles;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+import org.grep4j.core.model.Profile;
+
+public class ConsoleProfile extends Profile {
+
+	private Integer id;
+
+	@XmlAttribute
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@XmlAttribute
+	@Override
+	public String getName() {
+		return super.getName();
+	}
+
+	@Override
+	public void setName(String name) {
+		super.setName(name);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConsoleProfile other = (ConsoleProfile) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
